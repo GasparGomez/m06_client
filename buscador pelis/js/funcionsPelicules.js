9c84b8555f95b21980526da29e7677366a7fbd69
GasparGomez <a21gasgomtor@inspedralbes.cat>
2022-11-11 10:55:43 +0100

    uwu

--- buscador pelis/js/funcionsPelicules.js
+++ buscador pelis/js/funcionsPelicules.js
@@ -1,27 +1,16 @@
-
-
 Vue.component('buscador-peliculas', {
-    data: function() {
-        return {
-            busqueda: '',
-            resultado: []
-        }
-    }, 
+    data: function () {
+        return {busqueda: 'cars', resultado: []}
+    },
     template: `
     <div>
         <b-form-input v-model="busqueda" placeholder="Pon el titulo a buscar"></b-form-input>
         <b-button @click="buscar" variant="success">Buscar</b-button>
-        <ul>
-            <li v-for="peli in resultado">
-                {{peli.Title}}
-            </li>
-        </ul>
+        <cards-peliculas></cards-peliculas>
     </div>`,
     methods: {
-        buscar: function(){
-            fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2f34fcfe&s=' + this.busqueda)
-            .then((response) => response.json())
-            .then(data => {
+        buscar: function () {
+            fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2f34fcfe&s=' + this.busqueda).then((response) => response.json()).then(data => {
                 console.log(data);
                 this.resultado = data.Search;
             });
@@ -30,6 +19,17 @@ Vue.component('buscador-peliculas', {
 
 })
 
-let app = new Vue( {
-    el:'#app'
-});
+Vue.component('cards-peliculas', {
+    data: function () {},
+    template: `
+    <div>
+        <ul>
+            <li v-for="peli in resultado">
+                {{peli.Title}}
+            </li>
+        </ul>
+    </div>`,
+    methods: {}
+})
+
+let app = new Vue({el: '#app'});
