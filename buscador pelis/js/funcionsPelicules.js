e6182e4f3bedea464144e85af68a39584d4b46d6
GasparGomez <a21gasgomtor@inspedralbes.cat>
2022-11-16 12:52:47 +0100

    zona privada

--- buscador pelis/js/funcionsPelicules.js
+++ buscador pelis/js/funcionsPelicules.js
@@ -1,18 +1,29 @@
-Vue.component('buscador-peliculas', {
+const Buscador = Vue.component('buscador-peliculas', {
     data: function () {
-        return {busqueda: 'cars', resultado: []}
+        return {busqueda: 'cars', resultado: [], infoDetallada: false}
     },
     template: `
     <div>
         <b-form-input v-model="busqueda" placeholder="Pon el titulo a buscar"></b-form-input>
         <b-button @click="buscar" variant="success">Buscar</b-button>
+        <info-detallada v-show="infoDetallada.mostrar" :info="infoDetallada"></info-detallada>
         <b-row>
             <b-col md="3" v-for="peli in resultado">     
-                <card-pelicula :datos=peli></card-pelicula>
+                <card-pelicula @evtMasInformacion='buscarInformacion' :datos=peli></card-pelicula>
             </b-col>
         </b-row>
+        
     </div>`,
     methods: {
+        buscarInformacion: function (id) {
+            this.infoDetallada.mostrar = true;
+            fetch("https://www.omdbapi.com/?apikey=2f34fcfe&i=" + id)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.infoDetallada.datos = data;
+                });
+        },
         buscar: function () {
             fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2f34fcfe&s=' + this.busqueda).then((response) => response.json()).then(data => {
                 this.resultado = data.Search;
@@ -21,6 +32,26 @@ Vue.component('buscador-peliculas', {
     }
 
 })
+
+const Admin = Vue.component('zona-privada', {
+    data: function(){
+        return {
+            pelicules: []
+        }
+    },
+    template: `
+    <h3>zona privada</h3>
+    `
+})
+
+Vue.component('info-detallada', {
+    props: ['info'],
+    template: `<div>
+        <h3>Información detallada: </h3>
+        {{info.Plot}}
+        <b-button @click="info.mostrar=false">Cerrar</b-button>
+    </div>`,
+});
 
 Vue.component('login', {
     data: function () {
@@ -51,7 +82,9 @@ Vue.component('login', {
     methods: {
         submitLogin: function () {
             this.isloading = true;
-            fetch('http://alvaro.alumnes.inspedralbes.cat/loginGET.php?username=' + this.form.usuari + '&pwd=' + this.form.contrasenya).then((response) => response.json()).then(data => {
+            fetch('http://alvaro.alumnes.inspedralbes.cat/loginGET.php?username=' + this.form.usuari + '&pwd=' + this.form.contrasenya)
+            .then((response) => response.json())
+            .then(data => {
                 if (data.exito) {
                     this.logged = true;
                     this.imgSrc = data.imagen;
@@ -76,28 +109,23 @@ Vue.component('card-pelicula', {
     <b-card border-variant="secondary" :header="datos.Title" header-border-variant="secondary" align="center">
     <div>
         <b-img thumbnail fluid :src="datos.Poster" :alt="datos.Title"></b-img>
-        <b-button pill @click="buscarInfo" variant="outline-dark">Mes informacio</b-button>
+        <b-button class="button-orange" @click="$emit('evtMasInformacion', datos.imdbID)">Mas información</b-button>
+        <b-button class="button-orange" pill @click="buscarInfo" variant="outline-dark">Afegir</b-button>
     </div>
     </b-card>`,
     methods: {
-        buscarInfo: function () {}
+        buscarInfo: function () {},
+        enviarDades: function() {
+            
+        }
     }
 })
-
-// 1. Define route components.
-const Buscador = {
-    template: `
-    <buscador-peliculas></buscador-peliculas>`
-}
-const Admin = {
-    template: '<h3>admin uwu</h3>'
-}
 
 // 2. Define some routes
 // Each route should map to a component.
 const routes = [
     {
-        path: '/buscador',
+        path: '/',
         component: Buscador
     }, {
         path: '/admin',
