9961dd6f22c91daa6f8742dd9644ad2b76d8217a
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-01-11 10:33:10 +0100

    reset pelis

--- Pelicules amb vueNode/pelicules-vue/src/components/cardPelicula.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="datos.Poster" alt="datos.Title">
-        <div class="card-body">
-            <slot></slot>
-        </div>
-    </div>
-</template>
--- Pelicules amb vueNode/vue-project/README.md
+++ Pelicules amb vueNode/vue-project/README.md
@@ -1,4 +1,4 @@
-# pelicules-vue
+# vue-project
 
 This template should help get you started developing with Vue 3 in Vite.
 
