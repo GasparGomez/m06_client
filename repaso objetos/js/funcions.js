2543599b5348c4a1a5f610e44abbdd87299f0b2a
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-03-01 12:42:24 +0100

    xd

---
+++ repaso objetos/js/funcions.js
@@ -0,0 +1,9 @@
+class Empleado {
+    constructor() {
+        //  nombre: tipo cadena (Debe ser nombre y apellido) 
+        //  dni: tipo cadena 
+        //  edad : entero (Rango entre 18 y 45 años) 
+        //  casado: boolean . Si no pasan dato, tiene que tener por defecto el valor false
+        //  salario: tipo numérico doble. Si no pasan dato, tiene que tener por defecto el valor 60.000
+    }
+}
