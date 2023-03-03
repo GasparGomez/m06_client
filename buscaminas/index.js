b98d04385d354e2118c8ac2e393bdf7a418b5b3b
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-03-03 11:36:40 +0100

    a

--- buscaminas/index.js
+++ buscaminas/index.js
@@ -1,6 +1,6 @@
 let tablero = []
 const probabildadMina = 30;
-const N = 5;
+const N = 10;
 let tableroId = document.getElementById("tablero")
 
 
@@ -19,7 +19,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function renderTablero(tablero) {
+function renderTablero() {
     let htmlStr = ""
     let id = 0;
     for (let y = 0; y < N; y++) {
@@ -28,7 +28,43 @@ function renderTablero(tablero) {
         }
         for (let x = 0; x < N; x++) {
             htmlStr += "<td>"
-            htmlStr += `<img src="img/square.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+            switch (tablero[y][x]) {
+                case 0:
+                    htmlStr += `<img src="img/open0.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 1:
+                    htmlStr += `<img src="img/open1.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 2:
+                    htmlStr += `<img src="img/open2.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 3:
+                    htmlStr += `<img src="img/open3.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 4:
+                    htmlStr += `<img src="img/open4.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 5:
+                    htmlStr += `<img src="img/open5.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 6:
+                    htmlStr += `<img src="img/open6.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 7:
+                    htmlStr += `<img src="img/open7.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case 8:
+                    htmlStr += `<img src="img/open8.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+                case -5:
+                    htmlStr += `<img src="img/mina.png" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+
+                default:
+                    htmlStr += `<img src="img/square.gif" width="25px" height="25px" id="${[y, x].toString()}"/>`
+                    break;
+            }
+
             htmlStr += "</td>"
             id++;
         }
@@ -37,11 +73,11 @@ function renderTablero(tablero) {
         }
 
     }
-    console.log(tablero.toString());
+    // console.log(tablero.toString());
     tableroId.innerHTML = htmlStr
 }
 
-renderTablero(tablero)
+renderTablero()
 document.getElementById("tablero").addEventListener("click", (e) => {
     if (e.target.id != "" && e.target != tableroId) {
         let posicio = e.target.id.split(",")
@@ -51,6 +87,9 @@ document.getElementById("tablero").addEventListener("click", (e) => {
             tablero[posicio[0]][posicio[1]] = -5
         }
         let mines = contarMines(posicio);
+        tablero[posicio[0]][posicio[1]] = mines
+        console.log(mines);
+        renderTablero()
     }
 
 })
@@ -60,40 +99,14 @@ function contarMines(pos) {
 
     let [y, x] = pos
 
-    if (y > 0) {
-        if (x > 0 && tablero[y - 1][x - 1] == -9) {
-            contMines++;
+    for (let i = y - 1; i <= y + 1; i++) {
+        for (let j = x - 1; j <= x + 1; j++) {
+            if (i >= 0 && i < N && j >= 0 && j < N) {
+                if (tablero[i][j] == -9 || tablero[i][j] == -5) {
+                    contMines++;
+                }
+            }
         }
-
-        if (tablero[y - 1][x] == -9) {
-            contMines++;
-        }
-
-        if (tablero[y - 1][x + 1] == -9) {
-            contMines++;
-        }
-    }
-
-    if (y < N) {
-        if (x > 0 && tablero[y + 1][x - 1] == -9) {
-            contMines++;
-        }
-
-        if (tablero[y + 1][x] == -9) {
-            contMines++;
-        }
-
-        if (x < N && tablero[y + 1][x + 1] == -9) {
-            contMines++;
-        }
-    }
-
-    if (x > 0 && tablero[y][x - 1] == -9) {
-        contMines++;
-    }
-
-    if (x < N && tablero[y][x + 1] == -9) {
-        contMines++;
     }
 
 
